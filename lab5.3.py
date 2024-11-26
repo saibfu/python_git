d6694dca02297a5fda2261767141f7b492ca0035
siobfuu <siobfuuu@gmail.com>
2024-11-26 12:45:19 +0300

    добавлен лабораторая 5.3

--- lab5.3.py
+++ lab5.3.py
@@ -1,8 +1,32 @@
-import re
 
-line = input("Введите имя файла: ")
 
-if re.fullmatch(r"[a-zA-Z0-9а-яА-Я.]+([txt]|[doc]|[docx]|[odt]|[rtf])", line):
-    print(line, 'может быть именем файла')
-else:
-    print(line, 'не может быть именем файла')
+def game():
+    count = random.randint(4, 30)
+    print(f"Начальное количество камней {count}")
+
+    while count > 1:
+        while 1:
+            try:
+                user_move = int(
+                    input(f"У вас {count} камней. Введите количество камней (1, 2 или 3), которое вы хотите забрать: "))
+            except ValueError:
+                print("Введено неверное значение")
+                continue
+            else:
+                break
+
+        count -= user_move
+        print(f"Осталось {count} камней")
+        if count == 1:
+            print("Вы выиграли!")
+            break
+
+        computer_move = random.randint(1, 3)
+        count -= computer_move
+        print(f"Компьютер взял {computer_move} камней. Осталось {count} камней.")
+
+        if count == 1:
+            print("Компьютер выиграл!")
+
+
+game()
